8bf31ef2ac1df2aad74b168e2be8723f2d999896
J.G.Adams <82924084+JG-Adams@users.noreply.github.com>
2025-03-21 02:30:06 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,8 @@
 #include "ollama.hpp"
 #include "RAG.h"
 
-
-
 const std::string EMBEDDING_MODEL = "hf.co/CompendiumLabs/bge-base-en-v1.5-gguf";
-//const std::string EMBEDDING_MODEL = "llama3.2:latest";
-//const std::string EMBEDDING_MODEL = "deepseek-r1:1.5b";
 const std::string LANGUAGE_MODEL = "llama3.2:latest";
-//const std::string LANGUAGE_MODEL = "deepseek-r1:1.5b";
 
 // Each entry in the vector database is a pair of (chunk, embedding vector)
 std::vector<std::pair<std::string, std::vector<float>>> RAG_DATABASE;
@@ -50,7 +45,6 @@ int main() {
             ollama::request request(ollama::message_type::chat);
             request["model"]=LANGUAGE_MODEL;
             request["messages"]= messages.to_json();
-            //request["options"]["stream"] = true;
             request["stream"] = true;
             ollama::chat(request, [](const ollama::response& response) {
                 std::cout << response << std::flush;
@@ -58,21 +52,6 @@ int main() {
                     AI_busy = false;
                 }
             });
-            /*ollama::options options;
-            options["stream"] = true;
-
-            std::cout << "\nChatbot response:" << "\n";
-            //std::thread new_thread( [&]{ 
-                ollama::chat(LANGUAGE_MODEL, messages, [](const ollama::response& response) {
-                    std::cout << response << std::flush;
-                    if (response.as_json()["done"]==true) {
-                        AI_busy = false;
-                    }
-                }, options);*/
-            //} );
-            // Prevent the main thread from exiting while we wait for an asynchronous response.
-            //while (AI_busy) { std::this_thread::sleep_for(std::chrono::microseconds(100) ); }
-            //new_thread.join();
             std::cout << "\n";
         }else{
             continue;
