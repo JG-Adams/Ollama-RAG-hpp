9f821d7705dbcb7a1fea8c73c57a6cf94f085b9f
JG-Adams <JohnAdamsonfacebook@gmail.com>
2025-04-17 01:09:20 -0400

    modified:   CMakeLists.txt 	modified:   example.cpp 	modified:   include/RAG.hpp

--- example.cpp
+++ example.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2025 J.G.Adams
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <iostream>
 #include <thread>
 #include <atomic>
@@ -9,10 +33,12 @@ const std::string LANGUAGE_MODEL = "llama3.2:latest";
 
 // Each entry in the vector database is a pair of (chunk, embedding vector)
 std::vector<std::pair<std::string, std::vector<float>>> RAG_DATABASE;
-std::atomic_bool AI_busy = false;
+std::atomic_bool AI_busy{false};
+
+using namespace ollama::RAG;
 
 int main() {
-    RAG_loadDocument(RAG_DATABASE, EMBEDDING_MODEL, "cat-facts.txt");
+    RAG_loadDocument_ByLine(RAG_DATABASE, EMBEDDING_MODEL, "cat-facts.txt");
     std::cout << "Loaded " << RAG_DATABASE.size() << " entries." << "\n";
 
     while(true){
@@ -23,7 +49,8 @@ int main() {
             if (input_query == "quit"){ break; }
             AI_busy = true;
 
-            auto retrieved_knowledge = RAG_retrieve(RAG_DATABASE, EMBEDDING_MODEL, input_query);
+            const size_t fetchCount = 3;
+            auto retrieved_knowledge = RAG_retrieve(RAG_DATABASE, EMBEDDING_MODEL, input_query, fetchCount);
 
             std::cout << "Retrieved knowledge:" << "\n";
             for (const auto& [chunk, similarity] : retrieved_knowledge) {
